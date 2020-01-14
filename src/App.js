8d8d485adadc68c78fb523a56425b8d138e0529d
Gurpreet Singh <birdi.gpsingh@gmail.com>
2020-01-14 10:39:27 +0530

    direction updated

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
 
       <section className="map1 cid-qIiXmYb55q" id="map1-b">
         <div className="google-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.9945355627974!2d75.79656064398192!3d30.690429837191903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39107b614429c59d%3A0xb3dab395ea6af529!2sAhmedgarh%20-%20Dhulkot%20Rd%2C%20Punjab!5e0!3m2!1sen!2sin!4v1578914063777!5m2!1sen!2sin" width="600" height="450" frameborder="0" style={{ "border": "0px" }} allowfullscreen=""></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.0763406942588!2d75.81111961840196!3d30.688128105506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x292af8552d013d88!2sMehtab%20Palace!5e0!3m2!1sen!2sin!4v1578978424511!5m2!1sen!2sin" width="600" height="450" frameborder="0" style={{ "border": "0px" }} allowfullscreen=""></iframe>  
         </div>
       </section>
 
